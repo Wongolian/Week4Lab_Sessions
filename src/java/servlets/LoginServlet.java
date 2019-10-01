2abead3d58ef0817a099938e0cd8717a9f943fc4
Steven <Steven@Laptop-XPS15>
2019-10-01 09:52:53 -0600

    Week 4 Lab Starting

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -21,15 +22,32 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         getServletContext().getRequestDispatcher("/WEB-INF/Login.jsp").forward(request, response);
-        
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
+        HttpSession session = request.getSession();
+//        request.setAttribute("success", null);
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (username.equals("") || username == null || password.equals("") || password == null) {
+            request.setAttribute("required", "All fields are required, please try again");
+
+            getServletContext().getRequestDispatcher("/WEB-INF/Login.jsp").forward(request, response);
+            return;
+        }
+
+        session.setAttribute("username", username);
+
+        getServletContext().getRequestDispatcher("/WEB-INF/home.jsp")
+                .forward(request, response);
     }
 
     @Override
